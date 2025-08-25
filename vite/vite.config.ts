c7a61b208c4f2c40056c1c1a7864b8656e729cf8
Lyall <lyall@iriesoft.net>
2025-08-25 23:22:32 +0200

    up

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -3,10 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: "vite",
+  //root: "vite",
   plugins: [vue()],
   build: {
     outDir: "../public",
-    //emptyOutDir: true,
   },
 });
